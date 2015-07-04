3f5e17e01952d1b79da66efc434ea0f1ff47dd76
smongey <sean@aokstudio.com>
2015-07-04 16:47:53 +0100

    home, events, news, category and perm reminders pages

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,33 @@
 /* jshint devel:true */
-console.log('\'Allo \'Allo!');
-
 $(document).foundation();
+
+$('.search span.icon-search').on('click', function(){
+	var $input = $(this).siblings('input')
+	$input.toggleClass('open');
+	if ($input.hasClass('open')) {
+		$input.focus();
+	} else {
+		$input.blur();
+	}
+});
+
+
+$('.view a').on('click', function(e){
+
+	e.preventDefault();
+	$(this).addClass('active')
+	$(this).siblings('a').removeClass('active')
+
+	var classname = $(this).children('span').attr('class'),
+		type = classname.split('icon-')[1];
+
+	$('.events').removeClass('list grid');
+	$('.events').addClass(type);
+
+});
+
+
+$('.categories > ul > li').on('click', function(){
+	console.log('hi');
+	$('.categories ul > ul, .categories span.icon-arrow').toggleClass('active');
+})
